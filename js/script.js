4b2a67f1079f42e2f4e92139973f3674409c1335
BiswasAntor <biswasantor20@gmail.com>
2023-11-04 21:05:07 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let body = document.querySelector("body");
+let heading = document.querySelector("#heading");
+let buttons = document.querySelectorAll(".button");
+buttons.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+      if(e.target.id === "grey"){
+        body.style.background = e.target.id;
+        heading.style.background = "orange"
+      }
+      else if(e.target.id === "green"){
+        body.style.background = e.target.id;
+          heading.style.background = "orange"
+      }
+      else if(e.target.id === "blue"){
+        body.style.background = e.target.id;
+          heading.style.background = "orange"
+      }
+      else if(e.target.id === "yellow"){
+        body.style.background = e.target.id;
+          heading.style.background = "orange"
+      }
+    })
+})
